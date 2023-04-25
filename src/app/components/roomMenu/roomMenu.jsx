8d455571434ad29efd20168a218cd3417744e24f
Jonah <jonahpageweinbaum@gmail.com>
2023-04-25 13:46:19 -0700

    Add array of rooms as options in the dropdown room selector

--- src/app/components/roomMenu/roomMenu.jsx
+++ src/app/components/roomMenu/roomMenu.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import './roomMenuStyle.scss';
+import { useState, useEffect } from 'react';
 
 export default function RoomMenu(props) {
+  // Hook to set rooms array
+  const [rooms, setRooms] = useState([]);
+
+  // TODO change dummy data to fetch from database
+  useEffect(() => {
+    setRooms(['room1', 'room2', 'room3']);
+  }, []);
+
   return (
     <nav id="roomMenu">
       <select name="rooms">
-        <option value="Living Room">Living Room</option>
+        {rooms.map((room) => {
+          return <option value={room}>{room}</option>
+        })}
       </select>
     </nav>
   );
