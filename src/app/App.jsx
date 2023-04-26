f78a1987bb67f1ce98841ad8a21851811cc44b1e
Brandon Yoon <122177926+brand0n-y00n@users.noreply.github.com>
2023-04-26 15:55:58 -0700

    second commit

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -14,40 +14,46 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  element={
-          // (() => {
-          //   // if user is not logged in, load Login component to require login
-          //   if (!loggedIn) {
-          //     return (
-          //       <>
-          //         <Header />
-          //         <Login setLoggedIn={setLoggedIn} />
-          //       </>
-          //     )
-          //   } else {
-          //     // if user is logged in, display homepage
-          //     return (
-          //       <div id="app">
-          //         <Header />
-          //         <MainPage />
-          //       </div>
-          //     );
-          //   }
-          // })()
-          <div id="app">
-            <Header />
-            <MainPage user={user}/>
-          </div>
-        } />
-        <Route path="/signup" element={
-          <>
-            <Header />
-            <Signup />
-          </>
-        } />
+        <Route
+          path="/"
+          element={
+            // (() => {
+            //   // if user is not logged in, load Login component to require login
+            //   if (!loggedIn) {
+            //     return (
+            //       <>
+            //         <Header />
+            //         <Login setLoggedIn={setLoggedIn} />
+            //       </>
+            //     )
+            //   } else {
+            //     // if user is logged in, display homepage
+            //     return (
+            //       <div id="app">
+            //         <Header />
+            //         <MainPage />
+            //       </div>
+            //     );
+            //   }
+            // })()
+            <div id="app">
+              <Header />
+              <MainPage user={user} />
+            </div>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <>
+              <Header />
+              <Signup />
+            </>
+          }
+        />
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- src/server/controllers/plantController.js
+++ src/server/controllers/plantController.js
@@ -94,38 +94,113 @@ plantController.deletePlant = async (req, res, next) => {
   //destructure username, room_name, plant_name from req.body
   const { username, room_name, species } = req.body;
   //throw eror if username, room_name, or species are not provided
-  if(!username || !room_name || !species) {
-    throw new Error('Username, room name, or plant name not provided')
+  if (!username || !room_name || !species) {
+    throw new Error('Username, room name, or plant name not provided');
   }
   try {
     //find user with username, throw error if user not found
-    const currentUser = await model.User.findOne({username});
-    if(!currentUser){
-      throw new Error('User not found')
+    const currentUser = await model.User.findOne({ username });
+    if (!currentUser) {
+      throw new Error('User not found');
     }
     //find room with corresponding currentUser with room_name
-    const currentRoom = currentUser.rooms.find(rooms => rooms.room_name === room_name)
-    if(!currentRoom){
-      throw new Error('Room not found')
+    const currentRoom = currentUser.rooms.find(
+      rooms => rooms.room_name === room_name
+    );
+    if (!currentRoom) {
+      throw new Error('Room not found');
     }
-    const currentPlant = currentRoom.plants.some(plant => plant.species === species)
-    if(!currentPlant) {
+    const currentPlant = currentRoom.plants.some(
+      plant => plant.species === species
+    );
+    if (!currentPlant) {
       throw new Error('Plant not found');
-    }else{
-      const result = await model.User.updateOne({username, room_name}, {$pull: {'rooms.$.plants': { species: species } } })
-      if(!result){
-        throw new Error('Error deleting plant from database')
-      }
-      return next();
     }
-  }
-
-  catch (err){
+    const result = await model.User.updateOne(
+      { username, room_name },
+      { $pull: { 'rooms.$.plants': { species: species } } }
+    );
+    if (!result) {
+      throw new Error('Error deleting plant from database');
+    }
+    return next();
+  } catch (err) {
     return next({
       log: 'plantController.deletePlant',
       message: { err: 'Error deleting current plant: ' + `${err}` },
-    })
+    });
   }
-}
+};
+
+plantController.movePlant = async (req, res, next) => {
+  const { username, room_move_from, room_move_to, species } = req.body;
+  // check if all necessary info are provided
+  if ((!username || !room_move_from, !room_move_to, !species)) {
+    return next({
+      log: 'plantController.movePlant',
+      message: { err: 'Missing input field' },
+    });
+  }
+  try {
+    // check if user exists in db
+    const user = await model.User.findOne({ username });
+    if (!user) {
+      throw new Error('User not found.');
+    }
+    // check if both rooms exists
+    const roomFrom = user.rooms.find(
+      rooms => rooms.room_name === room_move_from
+    );
+    const roomTo = user.rooms.find(
+      rooms => rooms.room_name === room_move_to
+    );
+    // check if the rooms exist
+    if (!roomFrom) {
+      throw new Error(`${room_move_from} does not exist.`);
+    }
+    if (!roomTo) {
+      throw new Error(`${room_move_to} does not exist. `);
+    }
+    // check if plant exists in the room to move from and not in room to move to
+    const plant = roomFrom.plants.find(plant => plant.species === species);
+    if (!plant) {
+      throw new Error(`${species} does not exist in ${room_move_from}`);
+    }
+    const plantExistsTo = roomTo.plants.some(
+      plant => plant.species === species
+    );
+    if (plantExistsTo) {
+      throw new Error(`${species} Already exist in ${room_move_to}`);
+    }
+    const resultRemove = await model.User.updateOne(
+      { username, 'rooms.room_name': room_move_from },
+      {
+        $pull: { 'rooms.$.plants': { species: species } },
+      }
+    );
+    if (!resultRemove) {
+      throw new Error(
+        `$error removing ${species} from ${room_move_from} in database`
+      );
+    }
+    const resultAdd = await model.User.updateOne(
+      { username, 'rooms.room_name': room_move_to },
+      {
+        $push: { 'rooms.$.plants': plant },
+      }
+    );
+    if (!resultAdd) {
+      throw new Error(
+        `$error removing ${species} to ${room_move_to} in database`
+      );
+    }
+    return next();
+  } catch (err) {
+    return next({
+      log: 'plantController.movePlant',
+      message: { err: 'Error moving the plant ' + `${err}` },
+    });
+  }
+};
 
 module.exports = plantController;
