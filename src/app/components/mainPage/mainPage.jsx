3a8477b9e0699505c2b1e663f7ed3c36bde0e3cb
Jonah <jonahpageweinbaum@gmail.com>
2023-04-25 22:01:09 -0700

    Load user's rooms from DB

--- src/app/components/mainPage/mainPage.jsx
+++ src/app/components/mainPage/mainPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './mainPageStyle.scss';
 import RoomMenu from '../roomMenu/roomMenu.jsx';
 import LowerContainer from '../lowerContainer/lowerContainer.jsx';
@@ -10,7 +10,16 @@ import LowerContainer from '../lowerContainer/lowerContainer.jsx';
 //render room component
 //render form container componenet
 
-function MainPage() {
+function MainPage(props) {
+  const { user } = props;
+  const [rooms, setRooms] = React.useState([]);
+
+  useEffect( async () => {
+    const response = await fetch(`http://localhost:3000/users/${user}`);
+    const data = await response.json();
+    setRooms(data.room);
+  }, []);
+  console.log("rooms", rooms);
   // Keep track of selected room in state to pass to room component
   const [selectedRoom, setSelectedRoom] = React.useState('');
 
