ae4186df5c510d69b2857841ddfb83ce5585a5e3
Brandon Yoon <122177926+brand0n-y00n@users.noreply.github.com>
2023-04-26 15:41:56 -0700

    first commit in brandon/verifyingAuthenitcation branch

--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+const cookieParser = require('cookie-parser')
 const app = express();
 const cors = require('cors');
 //require routers
@@ -9,7 +10,7 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.use('/users', usersRouter);
 
@@ -19,6 +20,7 @@ app.use('/users', usersRouter);
 // });
 // app.get('/')
 app.get('/', (req, res) => {
+
   res.send('hello world');
 });
 
